c0286fe2f18339979ef30303f30c5a8ec2c1ca02
patrickbrito95 <patrick.brito@euconquisto.com>
2023-08-24 11:27:23 -0300

    feat: implement check token get method on HOC

--- src/auth/withAuthentication.jsx
+++ src/auth/withAuthentication.jsx
@@ -1,20 +1,45 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 const withAuthentication = (Component) => {
+
     const WrapperComponent = (props) => {
+        const [isLogged, setIsLogged] = useState(false)
         const isAuthenticated = localStorage.getItem('token');
         const navigate = useNavigate();
+
+        useEffect(() => {
+            const checkToken = async () => {
+                try {
+                    const token = localStorage.getItem('token');
+
+                    const response = await axios.get('https://djbnrrib9e.execute-api.us-east-2.amazonaws.com/v1/token-check', {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    });
+
+                    if (response.data) {
+                        setIsLogged(true)
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+            };
+
+            checkToken()
+        }, [isAuthenticated])
 
         if (isAuthenticated) {
             return <Component {...props} />;
         }
 
         useEffect(() => {
-            if (!isAuthenticated) {
+            if (!isAuthenticated && !isLogged) {
                 return navigate('/login')
             }
-        }, [isAuthenticated])
+        }, [isAuthenticated, isLogged])
 
     };
 
