f76304505d043e873cf484038987765912929c50
patrickbrito95 <patrick.brito@euconquisto.com>
2023-08-28 14:00:20 -0300

    fix: handling route redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import MedicationList from './pages/MedicationList';
 import CreateMedication from './pages/CreateMedication';
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/medication-list" element={<MedicationList />} />
         <Route path="/create-medication" element={<CreateMedication />} />
